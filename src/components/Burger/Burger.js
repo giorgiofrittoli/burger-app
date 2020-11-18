26cf74d7a4c5de35bbfa5a6af6493bc24b254888
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-18 21:15:39 +0100

    default empty burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from "./Burger.module.css";
 
 const burger = (props) => {
 
-    const tfIngredient = Object.keys(props.ingredients)
+    let tfIngredient = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, index) => {
                 return <BurgerIngredient key={igKey + index} type={igKey} />
             });
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (tfIngredient.length === 0) {
+        tfIngredient = <p>Please start adding ingredients!</p>;
+    }
+    console.log(tfIngredient)
 
     return (
         <div className={classes.Burger}>
