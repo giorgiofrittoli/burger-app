7ce257fbe9c346086ad34c1279051a5307d7ca09
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-18 21:04:46 +0100

    dynmic ingredient builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from "./Burger.module.css";
 
 const burger = (props) => {
 
+    const tfIngredient = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, index) => {
+                return <BurgerIngredient key={igKey + index} type={igKey} />
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {tfIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
