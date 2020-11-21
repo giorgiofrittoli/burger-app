15f9c4ef2e1afad1552455b4456ea39fcbb98bd0
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-21 11:10:29 +0100

    more ingredient button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,7 +4,7 @@ import classes from "./BuildControls.module.css";
 import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
-    { label: "Salad", type: "label" },
+    { label: "Salad", type: "salad" },
     { label: "Bacon", type: "bacon" },
     { label: "Cheese", type: "cheese" },
     { label: "Meat", type: "meat" },
@@ -13,7 +13,11 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctr =>
-            <BuildControl key={ctr.label} label={ctr.label} />
+            <BuildControl
+                key={ctr.label}
+                label={ctr.label}
+                added={() => props.ingredientAdded(ctr.type)}
+            />
         )}
     </div>
 );
