bc2521ec450e2c0132b84cb1884faa2a625c6f48
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-21 14:39:11 +0100

    button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -22,6 +22,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Aux>
     );
 };
