b99ea8583c6101d6b7765ad08d8519d2b2302ccd
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-22 09:35:29 +0100

    toogle menu button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import classes from "./Toolbar.module.css";
 
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import DrawerToggle from "../SideDrawer/DrawerToggle/drawerToggle";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
