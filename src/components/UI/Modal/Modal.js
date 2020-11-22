d87a5c5d96d1ae56632d0f4bc928b11acdb6ab86
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-22 09:55:56 +0100

    block modal update when not shown

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
 
 import Aux from "../../../hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            style={{
-                transform: props.show ? "translateY(0)" : "translateY(-100v)",
-                opacity: props.show ? "1" : "0",
-                display: props.show ? "block" : "none"
-            }}
-            className={classes.Modal}>
-            {props.children}
-        </div>
-    </Aux>
+class modal extends Component {
 
-);
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+
+        return (
+            < Aux >
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    style={{
+                        transform: this.props.show ? "translateY(0)" : "translateY(-100v)",
+                        opacity: this.props.show ? "1" : "0",
+                        display: this.props.show ? "block" : "none"
+                    }}
+                    className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </Aux >
+        );
+
+    }
+}
 
 export default modal;
