b99f85e3c0400f64f6f0223e578d3d3cdca07daa
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-11-22 09:42:50 +0100

    fix modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,7 +11,8 @@ const modal = (props) => (
         <div
             style={{
                 transform: props.show ? "translateY(0)" : "translateY(-100v)",
-                opacity: props.show ? "1" : "0"
+                opacity: props.show ? "1" : "0",
+                display: props.show ? "block" : "none"
             }}
             className={classes.Modal}>
             {props.children}
