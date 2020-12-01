c7147b1a935de408a91b1f0a9b1f3368e23125d9
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-12-01 21:49:31 +0100

    fix interceptor init

--- src/hoc/withErrorHandling/withErrorHandler.js
+++ src/hoc/withErrorHandling/withErrorHandler.js
@@ -7,18 +7,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
     return class extends Component {
 
-        state = {
-            error: null
-        }
-
-        componentDidMount() {
+        constructor() {
+            super();
             axios.interceptors.request.use(request => {
                 this.setState({ error: null });
                 return request;
             });
             axios.interceptors.response.use(response => response, error => {
                 this.setState({ error: error });
+                return error;
             });
+        }
+
+        state = {
+            error: null
         }
 
         errorConfirmedHanlder = () => {
